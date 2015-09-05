e1991a5c2ded1b87eebf2211e097df2b62bbd076
shelune <em.hieunguyen@gmail.com>
2015-09-06 05:32:01 +0700

    New transition for footer . Footer done . Moving on to the 'be nice' section

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@ $(document).ready(function () {
         e.preventDefault();
     });
     $('.fullpage').fullpage({
+        verticalCentered: true,
+        scrollingSpeed: 700,
+        onLeave: function (index, nextIndex, direction) {
+            if (nextIndex === 0) {
+                return false;
+            }
+            if ((index === 1) && (direction === "down")) {
+                $('.arrow').animate({
+                    "bottom": "0px",
+                    opacity: 0
+                }, 300);
+            }
 
+            if ((index === 2) && (direction === "up")) {
+                $('.arrow').animate({
+                    "bottom": "20px",
+                    opacity: 1
+                }, 300);
+            }
+        }
     });
 });
